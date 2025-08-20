8210ae7917ce71e101292f9cbed0ef6540a381e9
mathias ngo <mathias@mathias-pro.>
2025-08-20 15:48:19 +0200

    wip

---
+++ prettier.config.js
@@ -0,0 +1,6 @@
+const conf = {
+  plugins: ["prettier-plugin-tailwindcss"],
+};
+
+/** @type {import('prettier').Config & import('prettier-plugin-tailwindcss').PluginOptions} */
+export default conf;
